90127b99947798cbe5cc06e1e00034352e2c4b39
dontwanttorevealname <harrymoxon08@gmail.com>
2025-02-05 13:31:20 +0000

    Test

--- SQL/Migrations/001_Squad.sql
+++ SQL/Migrations/001_Squad.sql
@@ -78,12 +78,23 @@ INSERT INTO members (member_id, member_role, member_rank, weapon_id) VALUES
 (6, 'Team Leader', 'Sergeant', 1),
 (7, 'Automatic Rifleman', 'Corporal', 2),
 (8, 'Designated Marksman', 'Lance Corporal', 4),
-(9, 'Rifleman', 'Lance Corporal', 1);
+(9, 'Rifleman', 'Lance Corporal', 1),
+-- Machine Gun Team
+(10, 'Machine Gun Squad Leader', 'Staff Sergeant', 1),
+-- Charlie Team
+(11, 'Machine Gunner', 'Corporal', 2),
+(12, 'Assistant Gunner', 'Lance Corporal', 1),
+-- Delta Team
+(13, 'Machine Gunner', 'Corporal', 2),
+(14, 'Assistant Gunner', 'Lance Corporal', 1);
+
 
 -- Test data for teams
 INSERT INTO teams (team_id, team_name, team_size) VALUES
 (1, 'Alpha', 4),
-(2, 'Bravo', 4);
+(2, 'Bravo', 4)
+(3, 'Charlie', 2),
+(4, 'Delta', 2);
 
 -- Test data for team_members
 INSERT INTO team_members (team_id, member_id) VALUES
@@ -96,17 +107,29 @@ INSERT INTO team_members (team_id, member_id) VALUES
 (2, 6),
 (2, 7),
 (2, 8),
-(2, 9);
+(2, 9),
+-- Charlie Team
+(3, 11),
+(3, 12),
+-- Delta Team
+(4, 13),
+(4, 14);
 
 -- Test data for groups
 INSERT INTO groups (group_id, group_name, group_size, group_nationality) VALUES
-(1, 'Ranger Rifle Squad', 9, 'United States of America');
+(1, 'Ranger Rifle Squad', 9, 'United States of America'),
+(2, 'Machine Gun Squad', 5, 'United States of America');
+
 
 -- Test data for group_members
 INSERT INTO group_members (group_id, member_id, team_id) VALUES
 (1, 1, NULL),      -- Squad Leader
 (1, NULL, 1),      -- Alpha Team
-(1, NULL, 2);      -- Bravo Team
+(1, NULL, 2),      -- Bravo Team
+(2, 10, NULL),     -- Machine Gun Squad Leader
+(2, NULL, 3),      -- Charlie Team
+(2, NULL, 4);      -- Delta Team
+
 
 -- Additional test queries
 -- 4. Show full squad composition with hierarchy
